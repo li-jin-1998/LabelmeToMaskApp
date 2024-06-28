9470f474454e140a78f0ac205f4fbed158c9e253
LiJin <1320439118@qq.com>
2024-06-28 13:34:01 +0800

    update.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from LabelmeToMaskApp import *
+import sys
+from PyQt5.QtWidgets import QApplication
+from LabelmeToMaskApp import LabelmeToMaskWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = LabelmeToMaskWindow()
     window.show()
-    app.aboutToQuit.connect(window.save_default_directories)
+    app.aboutToQuit.connect(window.save_default_directory)
     sys.exit(app.exec_())
